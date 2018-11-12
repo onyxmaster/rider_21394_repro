adfdd570587de6ce2b8fc3ed66109c1e8fbf8ada
Aristarkh Zagorodnikov <xm@drive.net>
2018-11-12 12:14:39 +0300

    Initial commit

---
+++ Tests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace rider_21394_repro
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.False(System.Diagnostics.Trace.UseGlobalLock);
+        }
+    }
+}
